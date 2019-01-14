25ac2c1fa3092cdc462b3254a332387701590640
enpsl <1390509500@qq.com>
2019-01-14 23:14:42 +0800

    script 配置

--- src/pages/ui/gallery.js
+++ src/pages/ui/gallery.js
@@ -28,7 +28,7 @@ export default class Gallery extends React.Component{
             >
                 <Card.Meta
                     title="React Admin"
-                    description="I Love Imooc"
+                    description="I Love Gallery"
                 />
             </Card>
         ))
