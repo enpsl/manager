b3aaad33860aa4c85f73a26c68f4ed8190761589
enpsl <1390509500@qq.com>
2019-01-14 23:11:57 +0800

    ui组件

--- src/router.js
+++ src/router.js
@@ -4,6 +4,13 @@ import App from "./App"
 import Admin from './admin.js';
 import Login from './pages/login';
 import Button from './pages/ui/buttons';
+import Modals from './pages/ui/modals';
+import Loadings from './pages/ui/loadings';
+import Notice from './pages/ui/notice';
+import Messages from './pages/ui/messages';
+import Tabs from './pages/ui/tabs';
+import Gallery from './pages/ui/gallery';
+import Carousel from './pages/ui/carousel';
 import NoMatch from './pages/nomatch';
 
 export default class IRouter extends React.Component{
@@ -15,6 +22,13 @@ export default class IRouter extends React.Component{
                         <Admin>
                             <Switch>
                                 <Route path="/admin/ui/buttons" component={Button}/>
+                                <Route path="/admin/ui/modals" component={Modals}/>
+                                <Route path="/admin/ui/loadings" component={Loadings}/>
+                                <Route path="/admin/ui/notification" component={Notice}/>
+                                <Route path="/admin/ui/messages" component={Messages}/>
+                                <Route path="/admin/ui/tabs" component={Tabs}/>
+                                <Route path="/admin/ui/gallery" component={Gallery}/>
+                                <Route path="/admin/ui/carousel" component={Carousel}/>
                                 <Route component={NoMatch}/>
                             </Switch>
                         </Admin>
